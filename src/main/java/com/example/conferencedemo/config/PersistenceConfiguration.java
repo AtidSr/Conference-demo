8276d4ad534412ed4613f1d7e60a3c0f9b77e4c9
Atid Sr <atid.sr@ku.th>
2020-02-15 14:28:44 +0700

    test

---
+++ src/main/java/com/example/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.conferencedemo.config;
+
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app")
+        .username("postgres")
+        .password("Welcome");
+        System.out.print("My custom datasource has been initialized and set");
+        return builder.build();
+    }
+}
