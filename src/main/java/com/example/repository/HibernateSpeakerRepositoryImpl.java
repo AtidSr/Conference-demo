10def8dd9e972ff7d82f2c8332f7aa147f2bab6e
Atid Sr <atid.sr@ku.th>
2020-02-18 12:08:07 +0700

    beanFactory

--- src/main/java/com/example/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/example/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,19 +1,25 @@
 package com.example.repository;
 
 import com.example.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+    @Autowired
+    private Calendar cal;
 
     public List<Speaker> findAll(){
         List<Speaker> speakers = new ArrayList<Speaker>();
         Speaker speaker = new Speaker();
         speaker.setFirstName("Bryan");
         speaker.setLastName("Hansen");
+
+        System.out.println("cal : " + cal.getTime());
 
         speakers.add(speaker);
 
